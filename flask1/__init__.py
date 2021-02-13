38f7aa394f49b8fc4a3482bd53fadbfd72a6de9a
bigbarin69 <prathik.prejith@gmail.com>
2021-02-13 18:34:11 +0400

    do not look at this!!!

--- flask1/__init__.py
+++ flask1/__init__.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import dotenv
 
 app = Flask(__name__)
+dotenv.load_dotenv(dotenv.find_dotenv()) 
 os.environ.get("FLASK_SECRET")
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
 db = SQLAlchemy(app)
