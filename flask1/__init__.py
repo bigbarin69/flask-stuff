2f2ad68ecd99af8357dad3c73ef7d8ddd7ce8046
bigbarin69 <prathik.prejith@gmail.com>
2021-02-15 14:59:35 +0400

    account page

--- flask1/__init__.py
+++ flask1/__init__.py
@@ -7,7 +7,7 @@ import dotenv
 
 app = Flask(__name__)
 dotenv.load_dotenv(dotenv.find_dotenv()) 
-os.environ.get("FLASK_SECRET")
+app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET")
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
